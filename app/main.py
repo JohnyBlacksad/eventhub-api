1282db2e6a0cb0b88caca1dbeefe84dbd89fa449
JohnyBlacksad <vergulofthedevil@gmail.com>
2026-03-11 01:48:28 +0500

    create docker compose yml and first local start

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 from app.database import db_client
 
 @asynccontextmanager
@@ -8,8 +8,21 @@ async def lifespan(app: FastAPI):
     yield
     await db_client.close()
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    title='EventHub API',
+    version='0.1.0',
+    lifespan=lifespan)
 
-@app.get('/api/v1/health')
-def help():
-    return {'message': 'ok'}
+@app.get('/api/v1/health', tags=['System'])
+async def health_check():
+    try:
+        await db_client._ping()
+        return {
+            'status': 'ok',
+            'components': {
+                'database': "connected",
+            }
+        }
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+                            detail=f'Database is down: {e}')
