dccffedc2d90603779e3bb92dc13cb11fd7420bd
JohnyBlacksad <vergulofthedevil@gmail.com>
2026-03-14 19:46:53 +0500

    create activation code pydantic model

--- app/schemas/activation_code.py
+++ app/schemas/activation_code.py
@@ -6,11 +6,16 @@ from app.schemas.enums.user_enums.users_status import UserRoleEnum
 
 class ActivationCodeBaseModel(BaseModel):
     code: str
-    role: UserRoleEnum = UserRoleEnum.ORGANIZER
+    role: UserRoleEnum
+
+class ActivationCodeCreateModel(BaseModel):
+    model_config = ConfigDict(populate_by_name=True)
+    role: UserRoleEnum
+    code: Optional[str] = None
 
 class ActivationCodeModelResponse(ActivationCodeBaseModel):
     model_config = ConfigDict(populate_by_name=True, from_attributes=True)
     id: str = Field(alias='_id')
-    is_used: bool = False
-    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
-    activated_at: Optional[datetime]
+    is_used: bool = Field(default=False, alias='isUsed')
+    created_at: datetime = Field(alias='createdAt')
+    activated_at: Optional[datetime] = Field(default=None, alias='activatedAt')
